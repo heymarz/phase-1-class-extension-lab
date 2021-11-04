c8a8ba2785fea0992efc26d5d719ec5bec6b1fcc
Marlena Tsang <Marlena345@gmail.com>
2021-11-04 11:44:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+class Polygon {
+  constructor(array){
+    this.array = array;
+  }
+  get countSides(){
+  return  this.array.length;
+  }
+  get perimeter(){
+    return this.array.reduce(function(total, num){return total + num });
+  }
+  class Triangle extends Polygon {
+    //inherits constructor from polygon
+    get isValid(){
+      if(this.array === 3){
+        return true
+      }
+    }
+  }
+}
