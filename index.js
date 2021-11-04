cc37728f3f9b348bccde0c98418fefa0ddb5e136
Marlena Tsang <Marlena345@gmail.com>
2021-11-04 14:47:27 -0400

    commit tree 2cef43fbe6d91d80fa8fb1f355adcb4605f6f9b8

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
 class Polygon {
-  constructor(array){
-    this.array = array;
+  constructor(sides){
+    this.sides = sides;
+    this.count = this.sides.length
   }
   get countSides(){
-  return  this.array.length;
+  return this.sides.length;
   }
   get perimeter(){
-    return this.array.reduce(function(total, num){return total + num });
+    return this.sides.reduce(function(total, num){return total + num });
   }}
+
   class Triangle extends Polygon {
     //inherits constructor from polygon
     get isValid(){
-      if(this.array === 3){
+      let a = this.sides[0];
+      let b = this.sides[1];
+      let c = this.sides[2];
+      if(a+b>c && a+c>b && b+c>a){
         return true
+      } else {
+        return false
       }
     }
   }
+
+  class Square extends Polygon {
+    get area(){
+      return (this.sides[0] * this.sides[1])
+    }
+    get isValid() {
+      let side1 = this.sides[0];
+      let side2 = this.sides[1];
+      let side3 = this.sides[2];
+      let side4 = this.sides[3];
+      if((side1 ==side2)&&(side1 == side3)&& (side1 == side4)){
+        return true
+      } else {
+        return false
+      }
+    }
+  }
+
